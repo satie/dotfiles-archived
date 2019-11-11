2104e6248febce471b2491d00b45c14a522a173f
Satie Sharma <satiesharma@gmail.com>
2019-11-11 15:14:38 -0500

    Cleaned up aws configuration

--- aws/env.zsh
+++ aws/env.zsh
@@ -1,8 +1,9 @@
-export EC2_HOME=/usr/local/aws
+# export EC2_HOME=/usr/local/aws
+export EC2_HOME=$HOME/Library/Python/3.7
 export AWS_CONFIG_HOME=$HOME/.aws
 export AWS_CONFIG_FILE=$AWS_CONFIG_HOME/config
 export AWS_CREDENTIAL_PROFILES_FILE=$AWS_CONFIG_HOME/config
-export AWS_ACCESS_KEY_ID=$(cat $AWS_CONFIG_HOME/access)
-export AWS_SECRET_ACCESS_KEY=$(cat $AWS_CONFIG_HOME/secret)
-export AWS_REGION=$(cat $AWS_CONFIG_HOME/region)
-export AWS_DEFAULT_REGION=$AWS_REGION
+# export AWS_ACCESS_KEY_ID=$(cat $AWS_CONFIG_HOME/access)
+# export AWS_SECRET_ACCESS_KEY=$(cat $AWS_CONFIG_HOME/secret)
+# export AWS_REGION=$(cat $AWS_CONFIG_HOME/region)
+# export AWS_DEFAULT_REGION=$AWS_REGION
